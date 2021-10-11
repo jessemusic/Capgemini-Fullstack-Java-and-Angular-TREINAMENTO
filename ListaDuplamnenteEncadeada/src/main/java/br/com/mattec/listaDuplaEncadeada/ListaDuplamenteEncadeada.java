a51b21917bebfb6086dfb9a41ece42de2a0e1d5b
jessemusic <jmc_cursos@hotmail.com>
2021-10-10 22:52:13 -0300

    lista duplamente encadeada

---
+++ ListaDuplamnenteEncadeada/src/main/java/br/com/mattec/listaDuplaEncadeada/ListaDuplamenteEncadeada.java
@@ -0,0 +1,21 @@
+package br.com.mattec.listaDuplaEncadeada;
+
+public class ListaDuplamenteEncadeada<T> {
+
+    private NoDuplo<T> primeiroNo;
+    private NoDuplo<T> ultimoNo;
+
+    private int tamanhoLista;
+
+    public ListaDuplamenteEncadeada(){
+        this.primeiroNo = null;
+        this.ultimoNo = null;
+        this.tamanhoLista = 0;
+    }
+
+    public int size(){
+        return tamanhoLista;
+    }
+
+
+}

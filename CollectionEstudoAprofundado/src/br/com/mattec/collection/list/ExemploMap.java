012041b621422d33ca58459c17953a2a38baae41
jessemusic <jmc_cursos@hotmail.com>
2021-10-12 23:59:32 -0300

    Interface Map e seus métodos, continuação

--- CollectionEstudoAprofundado/src/br/com/mattec/collection/list/ExemploMap.java
+++ CollectionEstudoAprofundado/src/br/com/mattec/collection/list/ExemploMap.java
@@ -1,8 +1,10 @@
 package br.com.mattec.collection.list;
 
 import java.lang.reflect.MalformedParameterizedTypeException;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class ExemploMap {
 
@@ -20,8 +22,31 @@ public class ExemploMap {
         System.out.println(carrosPolpulares.toString());
 
         System.out.println("Substitua a consumo do gol por 1q5,23 km/L:");
-        carrosPolpulares.put("gol", 15.2);
+        carrosPolpulares.put("gol", 16.1);
         System.out.println(carrosPolpulares);
+
+        System.out.println("Confira se o modelo tucson está no icionário de carros:" +
+                " " + carrosPolpulares.containsKey("tucson"));
+        System.out.println("Exiba o consumo do uno:" + carrosPolpulares.get("uno"));
+
+       // System.out.println("Exiba o terceiro modelo adicionado!...... não é possível encontrar o índice neste map");
+        System.out.println("exiba os modelos!");
+        Set<String> modelos = carrosPolpulares.keySet();
+        System.out.println("Exiba os modelos dos carros:");
+        System.out.println(modelos);
+        System.out.println("Qual o modelo mais eficiente: ");
+        Double consumoMaisEficiente = Collections.max(carrosPolpulares.values());
+        System.out.println(consumoMaisEficiente);
+        Set<Map.Entry<String, Double>> entries = carrosPolpulares.entrySet();
+        
+        String modelomjuitomaisEfieciente ="";
+        for (Map.Entry<String, Double> entry : entries) {
+            if(entry.getValue().equals(consumoMaisEficiente)) {
+                modelomjuitomaisEfieciente = entry.getKey();
+                System.out.println("modelo mais eficiente: " + modelomjuitomaisEfieciente + " --> " + consumoMaisEficiente);
+            }
+        }
+
 
     }
 }

857483815d3bae831f7c9e5cad2cd0f27cbd9ed1
jessemusic <jmc_cursos@hotmail.com>
2021-10-13 01:15:52 -0300

    Estudo e aplicação dos métodos desta interface

--- CollectionEstudoAprofundado/src/br/com/mattec/collection/list/ExemploMap.java
+++ CollectionEstudoAprofundado/src/br/com/mattec/collection/list/ExemploMap.java
@@ -1,10 +1,7 @@
 package br.com.mattec.collection.list;
 
 import java.lang.reflect.MalformedParameterizedTypeException;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class ExemploMap {
 
@@ -16,13 +13,13 @@ public class ExemploMap {
             put("gol", 14.4);
             put("uno", 15.6);
             put("mod1", 16.1);
-            put("hb20", 14.5);
+            put("hb20", 13.5);
             put("kwid", 15.6);
         }};
         System.out.println(carrosPolpulares.toString());
 
         System.out.println("Substitua a consumo do gol por 1q5,23 km/L:");
-        carrosPolpulares.put("gol", 16.1);
+        carrosPolpulares.put("gol", 17.1);
         System.out.println(carrosPolpulares);
 
         System.out.println("Confira se o modelo tucson está no icionário de carros:" +
@@ -46,7 +43,78 @@ public class ExemploMap {
                 System.out.println("modelo mais eficiente: " + modeloMuitoMaisEfieciente + " --> " + consumoMaisEficiente);
             }
         }
-      
+        System.out.println("--------------------modelo menos econômico--------------");
+        System.out.println("Exiba o modelo menos econômico:");
+
+        Double minConsumoEficiente = Collections.min(carrosPolpulares.values());
+        String modeloMenoEficiente = "";
+        for (Map.Entry<String, Double> entry : carrosPolpulares.entrySet()) {
+            if (entry.getValue().equals(minConsumoEficiente)) {
+                modeloMenoEficiente = entry.getKey();
+                System.out.println("Modelo menos eficiente: " + modeloMenoEficiente
+                        + "----> " + minConsumoEficiente);
+            }
+        }
+
+            System.out.println("Exiba a soma dos consumos:");
+
+            Iterator<Double> iterator = carrosPolpulares.values().iterator();
+            Double soma = 0d;
+            while(iterator.hasNext()){
+                soma+= iterator.next();
+            }
+            System.out.println("A soma dos consumos são: " + soma);
+
+            System.out.println("----------exibir a média-------------");
+
+            System.out.println("exiba a média dos consumos desta lista de carros: ");
+            System.out.println("A média é: " + soma/carrosPolpulares.size());
+
+            System.out.println("----------------remoção-----------------");
+            System.out.println("Remova os modelos com consumo igual a 15,6 km/L: ");
+            System.out.println(carrosPolpulares);
+            Iterator<Double> iterator1 = carrosPolpulares.values().iterator();
+            while(iterator1.hasNext()){
+                if(iterator1.next().equals(15.6)) iterator1.remove();
+            }
+            System.out.println(carrosPolpulares);
+
+        System.out.println("Exiba todos os carros na ordem que formam informados");
+        Map<String, Double> carrosPolpulares1 = new LinkedHashMap<>() {{
+            //Para estar dentro de um ordem des inserção, devemos usar o LinkedHashMap<>()
+            put("gol", 14.4);
+            put("uno", 15.6);
+            put("mod1", 16.1);
+            put("hb20", 13.5);
+            put("kwid", 15.6);
+        }};
+        System.out.println(carrosPolpulares1.toString());
+        System.out.println("Exiba a lista de carros ordenados pelo modelo:");
+        Map<String,Double> carrosPopulares2 = new TreeMap<>(carrosPolpulares1);
+        // Neste caso a ordenação é feito pelas chaves que são Strings, portanto o TreeMap irá
+        // devolver a lista ordenada das chaves por ordem alfabética.
+        System.out.println(carrosPopulares2.toString());
+
+
+        System.out.println("Apagar a lista de carros:");
+        System.out.println("Vamos apagar a primeira lista carroPopulares:");
+        System.out.println("Lista antes de apagar");
+        System.out.println(carrosPolpulares);
+
+        System.out.println("Primeira lista carroPopulares apagada:");
+        carrosPolpulares.clear();
+        System.out.println(carrosPolpulares);
+        System.out.println("Vamos conferir se está vazio");
+        System.out.println(carrosPolpulares.isEmpty());
+
+
+
+
+
+
+
+
+
 
 
     }
